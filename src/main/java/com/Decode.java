9cc2de743d372d133f80cbe9a84112c21edc2d73
DruSadeMumba <drusademumba@gmail.com>
2022-04-24 12:39:56 +0300

    Create decryption key test

--- src/main/java/com/Decode.java
+++ src/main/java/com/Decode.java
@@ -2,5 +2,14 @@ package com;
 
 public class Decode {
     private String codedText = "";
-    private int decodeKey;
+    int decodeKey (int decryptionKey){
+        if (decryptionKey < 0  ){
+            throw new IllegalArgumentException("Enter encryption key between 0 - 25");
+        } else if (decryptionKey > 25) {
+            throw new IllegalArgumentException("Enter encryption key between 0 - 25");
+        } else {
+            return decryptionKey;
+        }
+    }
+
 }
